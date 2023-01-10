17ffccf710e151e6bd3b9a14caa7069a1706fa37
TaeWooKim <dhkdtpdn@gmail.com>
2023-01-10 21:16:02 +0900

    2

---
+++ Study_SpringMail/src/main/java/kr/or/kosa/HomeController.java
@@ -0,0 +1,29 @@
+package kr.or.kosa;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "home";
+	}
+	
+	@GetMapping("/simpleEmail")
+	public String SimpleMail() {
+		return "simpleEmail";
+	}
+}
